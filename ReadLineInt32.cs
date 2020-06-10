08415e944fc5cf957dd12e5f7840dbef452409af
pan93412 <pan93412@gmail.com>
2020-06-11 01:43:48 +0800

    ReadLineInt32: add a try-catch block

--- ReadLineInt32.cs
+++ ReadLineInt32.cs
@@ -6,9 +6,23 @@ namespace GuessNumber
     {
         public static int ReadLine(string prompt = "")
         {
-            if (prompt != "") Console.Write(prompt);
-            var _tmp = Console.ReadLine();
-            return Convert.ToInt32(_tmp);
+            int returnVal = -1;
+
+            try
+            {
+                if (prompt != "") Console.Write(prompt);
+                var _tmp = Console.ReadLine();
+                returnVal = Convert.ToInt32(_tmp);
+            }
+            catch (System.FormatException e) {
+                Console.Error.WriteLine("\r\n" +
+                    "位置: ReadLineInt32.cs > Convert.ToInt32\r\n" +
+                    $"發生例外狀況: {e.Message}\r\n" +
+                    "已啟用自動復原機制 - 程式將繼續運作，" +
+                    "但可能發生例外行為。");
+            }
+
+            return returnVal;
         }
     }
 }
